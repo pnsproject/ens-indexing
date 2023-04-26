f37d22e045359ac0f3098a26aca3b5bbb39c5569
Cupnfish <cupnfish@icloud.com>
2023-04-26 17:52:31 +0800

    fix get registration

--- src/ethRegistrar.ts
+++ src/ethRegistrar.ts
@@ -150,6 +150,11 @@ export async function handleNameTransferred(
   if (registration.domain == null) {
     let em = await (store as unknown as { em: () => Promise<EntityManager> }).em();
     await em.queryRunner?.commitTransaction();
+    registration = await store.get(Registration, label);
+    if (registration == null) {
+      log.error(`handleNameTransferred registration not found: ${label}`);
+      return;
+    };
   }
 
   log.info(`JSON: ${JSON.stringify(registration.domain)}`);
